843a7814c7b0f5075b653a9b8620740cada79368
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-02-27 17:50:27 -0500

    style: insert whitespace

--- diskinfo.php
+++ diskinfo.php
@@ -1,21 +1,28 @@
 <?php
 
-$desired_columns = [1, 2, 3, 4, 5, 6];		// columns desired from the result of `df`
+// columns desired from the result of `df`
+$desired_columns = [1, 2, 3, 4, 5, 6];
 
-$disk_data = array();		// will be an array of associative arrays
+// will be an array of associative arrays
+$disk_data = array();
 
 foreach ($desired_columns as $column) {
+	
 	// run the command.
 	// double $$ because the second $ is for interpolating the variable $column into the string, and the first $ is part of the `print` command's syntax.
 	$shell_response = shell_exec("df | awk '{ print $$column}'");
+	
 	// remove the trailing line break (and any other whitespace at the end) from the response
 	$rtrimmed_response = rtrim($shell_response);
+	
 	// split response into array, on line breaks
 	$response_array = explode("\n", $rtrimmed_response);
+	
 	// shift off the first item in the array, the column header.
 	$key = array_shift($response_array);
+	
 	// modify the associative arrays within each specifically targeted array
-	// this is the re-mapping of columns to our desired format
+	// this is the actual re-mapping of columns to our desired format
 	foreach ($response_array as $row => $value) {
 		$disk_data[$row][$key] = $value;
 	}
